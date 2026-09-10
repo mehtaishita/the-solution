9fbb35ba2c0039834a03898d34263dc5080c5300
mehtaishita <ishitamehta33@outlook.com>
2026-09-09 23:34:49 -0500

    base structure to get started with

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+const story = ref('');
+const twelve = ref('');
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+      <p>10th Step: What's the story behind the resentment, fear, anger, excitement?</p>
+      <textarea v-model="story" />
+
+      <p>Pause and ask your higher power for the truth behind this resentment, fear, anger, excitement.</p>
+
+      <p>What are the character defects at play?</p>
+      <p>Selfish</p>
+      <p>Dishonest</p>
+      <p>Fear</p>
+      <p>Self-seeking</p>
+
+      <p>7th step prayer</p>
+
+      <p>Pause and ask your higher power how you can help the addict who stil suffers.</p>
+      <input v-model="twelve" />
   </main>
 </template>
 
